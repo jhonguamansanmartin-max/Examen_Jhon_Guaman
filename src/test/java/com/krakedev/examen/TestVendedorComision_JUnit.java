ee40ce796f52d0505c3a8568dc2568eaca8ec68a
jhonguamansanmartin-max <jhonguamansamartin@gmail.com>
2026-05-09 21:49:27 -0500

    vendedor comision y pruebas unitarias pasadas

---
+++ src/test/java/com/krakedev/examen/TestVendedorComision_JUnit.java
@@ -0,0 +1,16 @@
+package com.krakedev.examen;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.krakedev.examen.parte1.VendedorComision;
+
+public class TestVendedorComision_JUnit {
+	@Test
+	void retornoCorrecto_Metodo_calcularSueldo_VendedorComision() {
+		VendedorComision vendedor = new VendedorComision("12345");
+
+		assertEquals(0, vendedor.calcularSueldo(), 0.001);
+	}
+}
